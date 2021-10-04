e02766932736b828dd9d494c4fb09434ec16cc69
beataged <86911933+beataged@users.noreply.github.com>
2021-10-04 21:09:14 +0300

    03 exercise

---
+++ 3-DOM/03/main.js
@@ -0,0 +1,8 @@
+const h1DOM = document.querySelector('h1');
+h1DOM.classList.add('simba');
+
+const liDOM = document.querySelectorAll('li');
+
+for(let i = 0; i < liDOM.length; i++){
+liDOM[i].classList.add('darzove')
+};
