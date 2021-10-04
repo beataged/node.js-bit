3da649e35fda5332eec98e91ec651f3d5722e850
beataged <86911933+beataged@users.noreply.github.com>
2021-10-04 19:31:06 +0300

    01 exercise

---
+++ 3-DOM/js/main.js
@@ -0,0 +1,12 @@
+//const h1DOM = document.getElementsByTagName('h1')[0];
+//const h1DOM = document.querySelector('h1');
+//h1DOM.innerText = 'Labas vakaras!';
+
+// const pDOM = document.querySelector('p');
+// pDOM.innerText = 'Šiandien gražus oras!';
+
+const aDOM = document.querySelector('a');
+aDOM.innerText = 'Spausk mane';
+
+
+console.log(aDOM);
